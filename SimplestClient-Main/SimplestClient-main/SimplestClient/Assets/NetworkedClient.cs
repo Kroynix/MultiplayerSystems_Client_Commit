68e06a8c51aff7143bab97ca9eba5ea889c0571d
Nathan Nguyen <zhectx@gmail.com>
2021-10-08 17:43:51 -0400

    Script Change Port

--- SimplestClient-Main/SimplestClient-main/SimplestClient/Assets/NetworkedClient.cs
+++ SimplestClient-Main/SimplestClient-main/SimplestClient/Assets/NetworkedClient.cs
@@ -12,7 +12,7 @@ public class NetworkedClient : MonoBehaviour
     int reliableChannelID;
     int unreliableChannelID;
     int hostID;
-    int socketPort = 5491;
+    int socketPort = 3389;
     byte error;
     bool isConnected = false;
     int ourClientID;
